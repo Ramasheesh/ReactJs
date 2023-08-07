34b7d154227774c843069fef32460f4d6f52f8fc
RamAsheesh Chauhan <43158746+Ramasheesh@users.noreply.github.com>
2023-08-07 20:01:46 +0530

    static list show

--- ProjectModule/todoApp/todo-app/src/component/Todo.js
+++ ProjectModule/todoApp/todo-app/src/component/Todo.js
@@ -1,9 +1,26 @@
 import { Component } from "react";
 
 class Todo extends Component{
+    constructor(){
+        super();
+        this.state={
+            tasks:["A","B",'C',"D"],
+        }
+    }
     render(){
         return(
-            <div><h1>Hello World</h1></div>
+           <div>
+                <input type="text"/>
+                <button>Add Tasks</button>
+                <ul>
+                    {this.state.tasks.map((ele)=>(
+                        <li>
+                            <p>{ele}</p>
+                        </li>
+                    ))}
+                </ul>
+               
+           </div>
         )
     }
 }
