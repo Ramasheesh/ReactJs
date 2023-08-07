1bc887799bbb33ab36a0adbd6e9182b9cab8d347
RamAsheesh Chauhan <43158746+Ramasheesh@users.noreply.github.com>
2023-08-07 21:41:46 +0530

    todo app end

--- ProjectModule/todoApp/todo-app/src/component/Todo.js
+++ ProjectModule/todoApp/todo-app/src/component/Todo.js
@@ -4,18 +4,40 @@ class Todo extends Component{
     constructor(){
         super();
         this.state={
-            tasks:["A","B",'C',"D"],
+            // tasks:[{task:"A",id:"1"},{task:"B",id:"2"}],
+            tasks:[],
+            currentTask:""
         }
     }
+    handleChange = (ele)=>{
+       this.setState({
+        currentTask: ele.target.value
+       })
+    }
+    handleAddTask = ()=>{
+        this.setState({
+            tasks: [...this.state.tasks ,{task: this.state.currentTask , id: this.state.tasks.length+1}],
+            currentTask:""
+        })
+    }
+    handeDelete = (id)=>{
+        let newArr = this.state.tasks.filter((taskObj) =>{
+            return taskObj.id !== id;
+           })
+           this.setState({
+            tasks: [...newArr]
+           })
+        }
     render(){
         return(
            <div>
-                <input type="text"/>
-                <button>Add Tasks</button>
+                <input type="text" value={this.state.currentTask} onChange = {this.handleChange}/>
+                <button onClick={this.handleAddTask}>Add Tasks</button>
                 <ul>
-                    {this.state.tasks.map((ele)=>(
-                        <li>
-                            <p>{ele}</p>
+                    {this.state.tasks.map((taskObj)=>(
+                        <li id={taskObj.id}>
+                            <p>{taskObj.task}</p>
+                            <button onClick={()=> this.handeDelete(taskObj.id)}>Delete</button>
                         </li>
                     ))}
                 </ul>
