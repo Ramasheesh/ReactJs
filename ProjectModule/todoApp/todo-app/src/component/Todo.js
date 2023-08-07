9164eaff9385bac7bf204a79d98ddd360d27c415
RamAsheesh Chauhan <43158746+Ramasheesh@users.noreply.github.com>
2023-08-07 17:42:49 +0530

    reactd demo start

---
+++ ProjectModule/todoApp/todo-app/src/component/Todo.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+class Todo extends Component{
+    render(){
+        return(
+            <div><h1>Hello World</h1></div>
+        )
+    }
+}
+export default Todo;
